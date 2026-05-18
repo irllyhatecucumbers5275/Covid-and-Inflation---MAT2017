a9e6182b7d9b96c880a28d6616547d2f6e6f280a
Seokhyun Hong <hseokhyun99@gmail.com>
2026-05-18 10:00:33 +0900

    Refactor comments and tidy up code structure

--- 0605_Night_Workflow Status.R
+++ 0605_Night_Workflow Status.R
@@ -2,8 +2,8 @@
 # 20년도와 23년도 데이터샘플을 추출 후 비교 필요
 
 # 우리가 할거는 2020 2023
-# 추출해서 비교하기. 일단 그러면 데이터셋 두개가 필수적이다. 
- # 인덱싱을 할 수 있으면 골드데이터도 하자. 
+# 추출해서 비교하기. 일단 그러면 데이터셋 두개가 필수
+ # 인덱싱을 할 수 있으면 골드데이터도
   
 # 골드 데이터 = gold
 # covid확진자 데이터 = covid01
@@ -13,19 +13,14 @@
 
   
 # 0605 2326 cpibmd0605_00.csv 생성완료
-
 # 0605 2341 goldbmd0605_00.csv 생성완료
-
 # 0605 23XX write_csv(lolcovid,"covidbmd0605_00.csv") 로 Covid dataset 생성완료
-
-
-
 
 
 --------------------------------
   
   
-%% 글로벌 모빌리티를 볼 것임
+# 글로벌 모빌리티를 봐야함
 
 > library(tidyverse)
 ── Attaching core tidyverse packages ────────────────────────────── tidyverse 2.0.0 ──
@@ -41,27 +36,30 @@
 > library(stringr)
 global_mobility.csv
 # A tibble: 11,730,025 × 9
-0. 열삭제삭제 (모빌리티리포트)
+# =====================================================
+# things to do  
+# 1. 열삭제 (모빌리티리포트)
 
-1. min max 조망. ((일단 보기만 할거임.))
-2. 절댓값 계산, mutate
-3. 열 삭제삭제
-4. 데이터프레임 mutate  (월별 합치는 연산)
-5. ((필요시 / / 열 삭제삭제))
+#2. min max 조망. ((일단 보기만 할거임.))
+#3. 절댓값 계산, mutate
+#4. 열 삭제삭제
+#5. 데이터프레임 mutate  (월별 합치는 연산)
+#6. ((필요시 / / 열 삭제삭제))
 
-6. 2020 추출
-7. 2023 추출
+#7. 2020 추출
+#8. 2023 추출
 
 
-2022까지밖에 없노!!
-  write_csv(gm0202,"mobile.csv") ㅋㅋㅋㅋㅋ 통제변인 떡칠 ㅋㅋㅋ
-from 2020 0201 to 2022 1001
+
+
+write_csv(gm0202,"mobile.csv")
+#from 2020 0201 to 2022 1001
 
 
 > write_csv(gm0202,"mobile_covidbmd0605_00.csv")
 
 https://support.google.com/covid19-mobility?hl=ko#topic=9822927 
-모빌리티 참고하시오
+# reference check for mobility data
 --------------
 
   
